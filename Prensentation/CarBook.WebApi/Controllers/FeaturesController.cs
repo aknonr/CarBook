21c83947dd4fb29c69b37d2b0c261c4e4f1dce5d
aknonr <133671359+aknonr@users.noreply.github.com>
2024-01-17 17:17:46 +0300

    Feature Api eklenmesi yapıldı....

---
+++ Prensentation/CarBook.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,65 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FeatureCommands;
+using CarBook.Application.Features.Mediator.Queries.FeatureQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FeaturesController(IMediator mediator)  //Yapıcı Method 
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> FeatureList()
+        {
+            // Burada _mediator nesnesi,  CQRS (Command Query Responsibility Segregation) deseni kullanılarak uygulama içindeki işlemleri yöneten bir mediatordur. GetFeatureQuery adında bir sorgu sınıfı oluşturulur ve bu sorgu mediatora gönderilir. await ifadesi, mediatordan gelen yanıtın tamamlanmasını bekler. Bu, asenkron bir işlemi bekletir ve kodun başka bir iş parçasını işlemesine izin verir.
+            var values = await _mediator.Send(new GetFeatureQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")] //Url kısmında ki id getirmesini sağlayacağız
+
+        public async Task<IActionResult> GetFeature(int id)
+        {
+            var value=await _mediator.Send(new GetFeatureByIdQuery(id)); //Get feature sınıfını artık id'ye göre getirebilirz
+            return Ok(value);
+        }
+
+
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateFeature(CreateFeatureCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Özellik Başarıyla Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveFeature(int id)
+        {
+            await _mediator.Send(new RemoveFeatureCommand(id));
+            return Ok("Özellik başarıyla silindi");
+        }
+
+        [HttpPut] //Bu attribute, UpdateFeature aksiyonunun HTTP PUT isteğine yanıt vereceğini belirtir. Yani, bu aksiyon bir kaynağı güncellemek için kullanılacaktır.
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Özellik başarıyla güncellendi");
+        }
+
+
+        //Mediator tasarım desenine ait CRUID işlemleri API ile birlikte yaptık.
+
+    }
+}
