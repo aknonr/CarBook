d470107ac495dc3e7a0b2ba481f1efcacf8ea02d
aknonr <133671359+aknonr@users.noreply.github.com>
2024-01-20 00:37:29 +0300

    View Compent için classlar açıldı ve fronted kısmında düzenlemeler yapıldı

--- Infrastructure/CarBook.Persistence/obj/Debug/net8.0/CarBook.Persistence.AssemblyInfo.cs
+++ Infrastructure/CarBook.Persistence/obj/Debug/net8.0/CarBook.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb1a0616d1a81171136e3f8834f6e8f73f6fc20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e503bc71bbc3d185ed22f3f88653f0d1008085")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
